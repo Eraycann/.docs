a48d26d2436f1dbdffdd023f0ab6334f363bddf1
Eraycan Sivri <eraycansivri@hotmail.com>
2024-08-06 16:42:07 +0300

    asdwqe

--- demo14/src/main/java/com/turkishjavadeveloper/MyController.java
+++ demo14/src/main/java/com/turkishjavadeveloper/MyController.java
@@ -1,5 +1,6 @@
 package com.turkishjavadeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MyController {
 	
+	// Field Injection (Alan Enjeksiyonu) kullanımı bu şekildedir.
+	// @Autowired annotation'ı ile Spring, IoC Container'dan gerekli bean'i (myComponent) alır ve bu alana (field) atar.
+	// @Lazy annotation'ı ise bu bean'in (myComponent) yaratılmasını geciktirir ve yalnızca gerçekten ihtiyaç duyulduğunda bean'i oluşturur.
+	@Autowired
+	@Lazy
 	private MyComponent myComponent;
 
 	// @Lazy annotation'ı ile MyController nesnesi oluşturulduğunda, myComponent field'ı henüz atanmaz.
 	// Bu field'a ihtiyaç duyulana kadar myComponent bean'i IoC Container'dan yüklenmez.
 	// Bu senaryoda, /name endpoint'i tetiklendiğinde injection işlemi gerçekleşir ve myComponent nesnesi atanır.
-	public MyController(@Lazy MyComponent myComponent) {
-		super();
-		this.myComponent = myComponent;
-	}
+		
+	// Alternatif olarak Constructor Injection ile @Lazy kullanımı bu şekildedir:
+	// public MyController(@Lazy MyComponent myComponent) {
+	//	 this.myComponent = myComponent;
+	// }
 	
 	@GetMapping("/name")
 	public String getComponentName() {
